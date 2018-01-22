27b4f3a022e573ededaed848c41c5166dd0830a5
unknown <alexander.fyh@gmail.com>
2018-01-22 14:27:06 -0300

    Por lo menos convierte a Matriz la tabla de Incidencia posterior

--- src/main/java/LectorPipe.java
+++ src/main/java/LectorPipe.java
@@ -26,7 +26,6 @@ public class LectorPipe {
         lectorPipe.incidenciaPosterior = tables.first();
         lectorPipe.incidenciaPrevia=tables.first().nextElementSibling();
         lectorPipe.incidencia=tables.first().nextElementSibling().nextElementSibling();
-        //System.out.println(lectorPipe.getArreglo(lectorPipe.incidenciaPosterior));
 
         Element arreglo = lectorPipe.getArreglo(lectorPipe.incidenciaPosterior);
         lectorPipe.getMatriz(arreglo);
@@ -40,28 +39,29 @@ public class LectorPipe {
         Element arreglo = tablas.select("tbody").first();
         return arreglo;
     }
-    public String[][] getMatriz(Element arreglo){
+    public Matriz getMatriz(Element arreglo){
+        try {
         Elements filas = arreglo.select("tr");
         Elements columnas = filas.first().select("td");
         System.out.println("Cantidad de filas = "+ filas.size());
         System.out.println("Cantidad de columnas = "+ columnas.size());
-        String[][] matriz = new String[filas.size()-1][columnas.size()-1];
+        int[][] matriz = new int [filas.size()-1][columnas.size()-1];
 
         for (int i = 1; i < filas.size(); i++) {
             columnas=filas.get(i).select("td");
             for (int j = 1; j <columnas.size() ; j++) {
-                matriz[i-1][j-1]=columnas.get(j).text();
+                matriz[i-1][j-1]=Integer.parseInt(columnas.get(j).text());
             }
         }
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[0].length; j++) {
-                System.out.print(matriz[i][j]);
-            }
-            System.out.println("");
+        Matriz matrix =  new Matriz(matriz);
+        matrix.imprimir();
+        return matrix;
+        }
+        catch(Exception e){
+            return null;
         }
 
-        //System.out.print(filas.get(28));
-        return matriz;
+
     }
     public LectorPipe(){
         try {
