7680a877768adecdfc264a772fa62d54cd1c9e2b
unknown <alexander.fyh@gmail.com>
2018-01-24 13:52:15 -0300

    lector que todavia no castea

---
+++ src/main/java/LectorTina.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class LectorTina {
+    private LectorPipe lectorPipe;
+    private File struct;
+    private List<String> LineasStruct;
+
+    public  LectorTina(LectorPipe lectorPipe){
+        this.LineasStruct = new ArrayList<String>();
+        FileReader fr = null;
+        BufferedReader br = null;
+        try {
+        this.lectorPipe = lectorPipe;
+            String direccion = (new File(".")).getCanonicalPath() + "//PetriNet//tinaNet-struct.txt";
+            this.struct = new File(direccion);
+
+            fr = new FileReader (struct);
+            br = new BufferedReader(fr);
+            String linea;
+            while((linea=br.readLine())!=null)
+                LineasStruct.add(linea);
+        }
+        catch (IOException e) { e.printStackTrace(); }
+        catch (Exception e){ e.printStackTrace();}
+        finally{
+            try{
+                if( null != fr ){
+                    fr.close();
+                }
+            }catch (Exception e2){
+                e2.printStackTrace();
+            }
+        }
+    }
+    public List<String> extraerLineas(String comienzo){
+        List<String> lineasExtraidas = new ArrayList<String>();
+        
+
+        return lineasExtraidas;
+    }
+
+    public static void main(String[] args){
+        LectorTina lectorTina = new LectorTina(new LectorPipe());
+        for (String linea :
+                lectorTina.LineasStruct) {
+            System.out.println(linea);
+        }
+    }
+}
